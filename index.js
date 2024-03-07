f131dcdcbbbbbb1224ff1a14770517feded427c0
mahmood-shamieh <mahmoodshamiehh2000@gmaail.com>
2024-03-07 14:06:57 +0300

    hello world

---
+++ index.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+
+// create a new MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'al_shamel'
+});
+// connect to the MySQL database
+connection.connect((error) => {
+    if (error) {
+        console.error('Error connecting to MySQL database:', error);
+    } else {
+        console.log('Connected to MySQL database!');
+    }
+});
+
+// close the MySQL connection
+app.get('/', function (req, res) {
+    res.send('<h1>hello world</h1>');
+});
+app.listen(5000, function () {
+    console.log("Express App running at http://127.0.0.1:5000/");
+})
+
+// connection.end();
